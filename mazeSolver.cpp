fca2ec187e42e9ffd5c9e8bdfa7eeb6e6a24f923
Nicholas Pahl <pahlni@mail.gvsu.edu>
2016-08-08 09:07:08 -0400

    Init commit with given files and sample mazes

---
+++ mazeSolver.cpp
@@ -0,0 +1,129 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int bruteForceMazeSolver(int i, int j);
+int backtrackingMazeSolver(int i, int j);
+int greedyMazeSolver(int i, int j);
+int divideAndConquerMazeSolver(int i, int j);
+int dynamicProgrammingMazeSolver(int i, int j);
+int randomizedMazeSolver(int i, int j);
+
+struct maze
+{
+	int rows;
+	int cols;
+	char matrix [100][100];
+};
+
+maze myMaze;
+
+int main()
+{
+	//required variables
+	ifstream in;
+	in.open("maze.txt");
+	char line;
+	
+	//read the matrix using plain c code, character by character
+	in >> myMaze.rows;
+	in >> line;
+	in >> myMaze.cols;
+	cout << "Reading a " << myMaze.rows << " by " << myMaze.cols << " matrix." << endl;
+	//Burn the end of line character
+	in.ignore(200,'\n');
+	for(int i=0; i<myMaze.rows; i++)
+	{
+		for(int j=0; j<myMaze.cols; j++)
+		{
+			in.get( myMaze.matrix[i][j] );
+		}
+		//Burn the end of line character
+		in.ignore(200,'\n');
+	}
+	
+	//Print the empty maze
+	for(int i=0; i<myMaze.rows; i++)
+	{
+		for(int j=0; j<myMaze.cols; j++)
+			cout << myMaze.matrix[i][j];
+		cout << endl;
+	}
+	int x=1,y=1;
+	
+	//Find starting coordinates
+	for(int i=0; i<myMaze.rows; i++)
+		for(int j=0; j<myMaze.cols; j++)
+			if( myMaze.matrix[i][j] == 'S' ){
+				x=i;
+				y=j;
+			}
+	
+	//Call a recursive mazeSolver
+    int bfDistance = bruteForceMazeSolver(x,y);     //brute force? dnc?
+    int btDistance = backtrackingMazeSolver(x,y);     //brute force? dnc?
+    int gDistance = greedyMazeSolver(x,y);
+    int dncDistance = divideAndConquerMazeSolver(x,y);
+    int dpDistance = dynamicProgrammingMazeSolverZZ(x,y);
+    int rDistance = randomizedMazeSolver(x,y);
+    
+    cout << "Brute force distance: " << bfDistance << " units away!" << endl;
+    cout << "Backtracking distance: " << btDistance << " units away!" << endl;
+    cout << "Greedy distance: " << gDistance << " units away!" << endl;
+    cout << "Divide and conquer distance: " << dncDistance << " units away!" << endl;
+    cout << "Dynamic programming distance: " << dpDistance << " units away!" << endl;
+    cout << "Randomized distance: " << rDistance << " units away!" << endl;
+    
+	//Print solved maze
+	for(int i=0; i<myMaze.rows; i++)
+	{
+		for(int j=0; j<myMaze.cols; j++)
+			cout << myMaze.matrix[i][j];
+		cout << endl;
+	}
+
+    return 0;
+}
+
+int bruteForceMazeSolver(int i, int j)
+{
+    //add algorithm here
+    return -1;
+}
+int backtrackingMazeSolver(int i, int j)
+{
+    //algorithm goes here
+    return -1;
+}
+int greedyMazeSolver(int i, int j)
+{
+    //algorithm goes here
+    return -1;
+}
+int divideAndConquerMazeSolver(int i, int j)
+{
+    //algorithm goes here
+    return -1;
+}
+int dynamicProgrammingMazeSolver(int i, int j)
+{
+    //algorithm goes here
+    return -1;
+}
+int randomizedMazeSolver(int i, int j)
+{
+    //algorithm goes here
+    return -1;
+}
+
+//recursion!!
+//Mark current location
+//Base Case: Look north, south, east, west for victory!
+//Mark our path
+//Try going south if it is open
+//Try going north if it is open
+//Try going east if it is open
+//Try going west if it is open
