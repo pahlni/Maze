02b1e0c1d785520e9b0ad99c54c2a7735ef7fa34
Nicholas Pahl <pahlni@mail.gvsu.edu>
2016-08-09 23:44:27 -0400

    added brute force and random functions

--- PahlProject4.cpp
+++ PahlProject4.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <time.h>
 
 using namespace std;
 
@@ -23,75 +24,119 @@ maze myMaze;
 
 int main()
 {
-	//required variables
-	ifstream in;
-	in.open("maze.txt");
-	char line;
-	
-	//read the matrix using plain c code, character by character
-	in >> myMaze.rows;
-	in >> line;
-	in >> myMaze.cols;
-	cout << "Reading a " << myMaze.rows << " by " << myMaze.cols << " matrix." << endl;
-	//Burn the end of line character
-	in.ignore(200,'\n');
-	for(int i=0; i<myMaze.rows; i++)
+  srand(time(NULL));
+  //seed random number generator
+  
+  //required variables
+  ifstream in;
+  in.open("maze.txt");
+  char line;
+  
+  //read the matrix using plain c code, character by character
+  in >> myMaze.rows;
+  in >> line;
+  in >> myMaze.cols;
+  cout << "Reading a " << myMaze.rows << " by " << myMaze.cols << " matrix." << endl;
+  //Burn the end of line character
+  in.ignore(200,'\n');
+  for(int i=0; i<myMaze.rows; i++)
+    {
+      for(int j=0; j<myMaze.cols; j++)
 	{
-		for(int j=0; j<myMaze.cols; j++)
-		{
-			in.get( myMaze.matrix[i][j] );
-		}
-		//Burn the end of line character
-		in.ignore(200,'\n');
+	  in.get( myMaze.matrix[i][j] );
 	}
-	
-	//Print the empty maze
-	for(int i=0; i<myMaze.rows; i++)
-	{
-		for(int j=0; j<myMaze.cols; j++)
+      //Burn the end of line character
+      in.ignore(200,'\n');
+    }
+  
+  //Print the empty maze
+  for(int i=0; i<myMaze.rows; i++)
+    {
+      for(int j=0; j<myMaze.cols; j++)
 			cout << myMaze.matrix[i][j];
-		cout << endl;
-	}
-	int x=1,y=1;
-	
-	//Find starting coordinates
-	for(int i=0; i<myMaze.rows; i++)
-		for(int j=0; j<myMaze.cols; j++)
-			if( myMaze.matrix[i][j] == 'S' ){
-				x=i;
-				y=j;
-			}
-	
-	//Call a recursive mazeSolver
-    int bfDistance = bruteForceMazeSolver(x,y);     //brute force? dnc?
-    int btDistance = backtrackingMazeSolver(x,y);     //brute force? dnc?
-    int gDistance = greedyMazeSolver(x,y);
-    int dncDistance = divideAndConquerMazeSolver(x,y);
-    int dpDistance = dynamicProgrammingMazeSolver(x,y);
-    int rDistance = randomizedMazeSolver(x,y);
-    
-    cout << "Brute force distance: " << bfDistance << " units away!" << endl;
-    cout << "Backtracking distance: " << btDistance << " units away!" << endl;
-    cout << "Greedy distance: " << gDistance << " units away!" << endl;
-    cout << "Divide and conquer distance: " << dncDistance << " units away!" << endl;
-    cout << "Dynamic programming distance: " << dpDistance << " units away!" << endl;
-    cout << "Randomized distance: " << rDistance << " units away!" << endl;
-    
-	//Print solved maze
-	for(int i=0; i<myMaze.rows; i++)
-	{
-		for(int j=0; j<myMaze.cols; j++)
-			cout << myMaze.matrix[i][j];
-		cout << endl;
-	}
-
-    return 0;
+      cout << endl;
+    }
+  int x=1,y=1;
+  
+  //Find starting coordinates
+  for(int i=0; i<myMaze.rows; i++)
+    for(int j=0; j<myMaze.cols; j++)
+      if( myMaze.matrix[i][j] == 'S' ){
+	x=i;
+	y=j;
+      }
+  
+  //Call a recursive mazeSolver
+  
+  maze temp = myMaze;
+  
+  int bfDistance = bruteForceMazeSolver(x,y);     //brute force? dnc?
+  
+  cout << "Brute force distance: " << bfDistance << " units away!" << endl;
+  
+  //Print solved maze
+  for(int i=0; i<myMaze.rows; i++)
+    {
+      for(int j=0; j<myMaze.cols; j++)
+	cout << myMaze.matrix[i][j];
+      cout << endl;
+    }
+  
+  
+  myMaze=temp;
+  int rDistance = randomizedMazeSolver(x,y);
+  cout << "Randomized distance: " << rDistance << " units away!" << endl;
+  
+  //Print solved maze
+  for(int i=0; i<myMaze.rows; i++)
+    {
+      for(int j=0; j<myMaze.cols; j++)
+	cout << myMaze.matrix[i][j];
+      cout << endl;
+    }
+  
+  return 0;
 }
 
 int bruteForceMazeSolver(int i, int j)
 {
-    //add algorithm here
-    return -1;
+  int ret;
+  
+  if(myMaze.matrix[i-1][j]=='F'||myMaze.matrix[i+1][j]=='F'||
+     myMaze.matrix[i][j-1]=='F'||myMaze.matrix[i][j+1]=='F'){
+    return 0;
+  }
+
+  if(myMaze.matrix[i-1][j]==' '){
+    myMaze.matrix[i-1][j]='.';
+    ret = bruteForceMazeSolver(i-1, j);
+    if(ret != -1){
+      return 1 + ret;
+    }
+  }
+  if(myMaze.matrix[i+1][j]==' '){
+    myMaze.matrix[i+1][j]='.';
+    ret = bruteForceMazeSolver(i+1, j);
+    if(ret != -1){
+      return 1 + ret;
+    }
+  }
+  if(myMaze.matrix[i][j-1]==' '){
+    myMaze.matrix[i][j-1]='.';
+    ret = bruteForceMazeSolver(i, j-1);
+    if(ret != -1){
+      return 1 + ret;
+    }
+  }
+  if(myMaze.matrix[i][j+1]==' '){
+    myMaze.matrix[i][j+1]='.';
+    ret = bruteForceMazeSolver(i, j+1);
+    if(ret != -1){
+      return 1 + ret;
+    }
+  }
+
+  return -1;
 }
 int backtrackingMazeSolver(int i, int j)
 {
@@ -115,8 +160,64 @@ int dynamicProgrammingMazeSolver(int i, int j)
 }
 int randomizedMazeSolver(int i, int j)
 {
-    //algorithm goes here
+  int ret;
+  int dir = rand() % 4;
+
+  if(myMaze.matrix[i-1][j]=='F'||myMaze.matrix[i+1][j]=='F'||
+     myMaze.matrix[i][j-1]=='F'||myMaze.matrix[i][j+1]=='F'){
+    return 0;
+  }
+
+  
+  switch(dir){
+  case 0:
+    if(myMaze.matrix[i-1][j]==' '){
+      myMaze.matrix[i-1][j]='.';
+      ret = randomizedMazeSolver(i-1, j);
+      if(ret != -1){
+	return 1 + ret;
+      }
+    }
+    break;
+  
+  case 1:
+    if(myMaze.matrix[i+1][j]==' '){
+      myMaze.matrix[i+1][j]='.';
+      ret = randomizedMazeSolver(i+1, j);
+      if(ret != -1){
+	return 1 + ret;
+      }
+    }
+    break;
+
+  case 2:
+    if(myMaze.matrix[i][j-1]==' '){
+      myMaze.matrix[i][j-1]='.';
+      ret = randomizedMazeSolver(i, j-1);
+      if(ret != -1){
+	return 1 + ret;
+      }
+    }
+    break;
+
+  case 3:
+    if(myMaze.matrix[i][j+1]==' '){
+      myMaze.matrix[i][j+1]='.';
+      ret = randomizedMazeSolver(i, j+1);
+      if(ret != -1){
+	return 1 + ret;
+      }
+    }
+    break;
+  }  
+  
+  if(myMaze.matrix[i-1][j]!=' '&&myMaze.matrix[i+1][j]!=' '&&
+     myMaze.matrix[i][j-1]!=' '&&myMaze.matrix[i][j+1]!=' '){
     return -1;
+  }
+  
+  return randomizedMazeSolver(i, j);
+
 }
 
 //recursion!!
